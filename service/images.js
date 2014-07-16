3c0191c0666258b1d81b5996e83e813da924e0a9
nospamas <jnospamas@gmail.com>
2014-07-16 17:43:36 +0100

    changed font sizes and artist only answers.

--- service/images.js
+++ service/images.js
@@ -1,98 +1,98 @@
 module.exports = [
     {
-        name: 'Blur: Parklife',
+        name: 'Blur',// Parklife',
         image: '0a774f59f54a341365b28f5871590487.jpg'
     },
     {
-        name: 'London Symphony Orchestra, The Who: Tommy',
+        name: 'The Who',//'London Symphony Orchestra, The Who',// Tommy',
         image: 'Screen Shot 2014-07-16 at 14.00.21.png'
     },
     {
-        name: 'Guns N\' Roses: Appetite For Destruction',
+        name: 'Guns N\' Roses',// Appetite For Destruction',
         image: '3d4a4cda8764b072f3f2b31cb532e48c.jpg'
     },
     {
-        name: 'Primal Scream: Screamadelica',
+        name: 'Primal Scream',// Screamadelica',
         image: '6cd01aa3c7f205d965f511cc27335cde.jpg'
     },
     {
-        name: 'Nirvana: Nevermind',
+        name: 'Nirvana',// Nevermind',
         image: '7c261561f6e32062d28f19faf8cc9bd2.jpg'
     },
     {
-        name: 'Bruce Springsteen: Born In The USA',
+        name: 'Bruce Springsteen',// Born In The USA',
         image: '33b5c30bfac8e026ce4327c4e4f68cbe.jpg'
     },
     {
-        name: 'Prince: Purple Rain',
+        name: 'Prince',// Purple Rain',
         image: '86a26e878f7009bbaabc79397d8a6850.jpg'
     },
     {
-        name: 'REM: Automatic For The People',
+        name: 'REM',// Automatic For The People',
         image: '86b044f2c97bef84524f0bb77ecada5f.jpg'
     },
     {
-        name: 'The Rolling Stones: Exile On Main St.',
+        name: 'The Rolling Stones',// Exile On Main St.',
         image: '636dbb5a16bc7c90129c8a85cb2b6f7f.jpg'
     },
     {
-        name: 'The Clash: London Calling',
+        name: 'The Clash',// London Calling',
         image: '737cf686d1d6f45fd3309262c35b1d79.jpg'
     },
     {
-        name: 'David Bowie: Aladdin Sane',
+        name: 'David Bowie',// Aladdin Sane',
         image: 'Screen Shot 2014-07-16 at 14.03.04.png'
     },
     {
-        name: 'Meat Loaf: Bat Out of Hell',
+        name: 'Meat Loaf',// Bat Out of Hell',
         image: '896cc6840bf45ae010044083d3c1ad66.jpg'
     },
     {
-        name: 'Led Zeppelin: Led Zeppelin IV',
+        name: 'Led Zeppelin',// Led Zeppelin IV',
         image: '30071af19bcab11dcba4dde065e350a2.jpg'
     },
     {
-        name: 'Michael Jackson: Thriller',
+        name: 'Michael Jackson',// Thriller',
         image: '4173885a20cb9ed4e3400637fba37f8d.jpg'
     },
     {
-        name: 'The Smiths: The Queen Is Dead',
+        name: 'The Smiths',// The Queen Is Dead',
         image: 'Screen Shot 2014-07-16 at 14.06.59.png'
     },
     {
-        name: 'The Velvet Underground & Nico',
+        name: 'The Velvet Underground', // & Nico',
         image: 'bf4637b7374821080f5495fd0dfc555c.jpg'
     },
     {
-        name: 'Mike Oldfield: Tubular Bells',
+        name: 'Mike Oldfield',// Tubular Bells',
         image: 'c9a2c92d2487942c8dd42388d0765fef.jpg'
     },
     {
-        name: 'Massive Attack: Blue Lines',
+        name: 'Massive Attack',// Blue Lines',
         image: 'cfd807a817fa535b4561d32abc593a68.jpg'
     },
     {
-        name: 'Radiohead: OK Computer',
+        name: 'Radiohead',// OK Computer',
         image: 'd70eb2cf97a4f228ef96f60a4c2e58ff.jpg'
     },
     {
-        name: 'Blondie: Parallel Lines',
+        name: 'Blondie',// Parallel Lines',
         image: 'Screen Shot 2014-07-16 at 14.09.28.png'
     },
     {
-        name: 'Red Hot Chili Peppers: Blood Sugar Sex Magik',
+        name: 'Red Hot Chili Peppers',// Blood Sugar Sex Magik',
         image: 'e080e8b03e7b286262405810f921d05f.jpg'
     },
     {
-        name: 'The Sex Pistols: Never Mind The Bollocks, Here\'s The Sex Pistols',
+        name: 'The Sex Pistols',// Never Mind The Bollocks, Here\'s The Sex Pistols',
         image: 'e874f3b6d90fd2c686f08e5c4a210566.jpg'
     },
     {
-        name: 'U2: Achtung Baby',
+        name: 'U2',// Achtung Baby',
         image: 'ebfd07f5ba495017f5cbd0c95402bef8.jpg'
     },
     {
-        name: 'Stevie Wonder: Songs In The Key Of Life',
+        name: 'Stevie Wonder',// Songs In The Key Of Life',
         image: 'f146fa5cc73203266ef23c822cbca537.jpg'
     }
 ];
